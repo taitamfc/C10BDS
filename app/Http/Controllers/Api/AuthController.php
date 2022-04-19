ad784f6f28291ccaf1f34bb5c89ab08480b7d36f
taitamfc <taitam8590@gmail.com>
2022-04-19 17:29:01 +0700

    fix bug 3

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use Illuminate\Support\Facades\Validator as Validator;
 
 class AuthController extends Controller
 {
