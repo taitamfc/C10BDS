f97ed727c62a629421f5077ec273badc535eafca
thuyka <thuyka225@gmail.com>
2022-04-27 10:46:19 +0700

    customers

---
+++ app/Models/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // add soft delete
+
+class Customer extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table = 'customers';
+    protected $fillable = [
+        'name', 'email', 'address', 'phone'
+    ];
+    
+}
