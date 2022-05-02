56e8d2f300fe90e8f29b0cf8b68c30febd7f49ae
taitamfc <taitam8590@gmail.com>
2022-05-02 21:30:20 +0700

    do tasks vue

--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -8,9 +8,9 @@
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="theme-color" content="#000000">
-    <title>QuangGroup CRM</title>
-    <meta name="description" content="Mobilekit HTML Mobile UI Kit">
-    <meta name="keywords" content="bootstrap 5, mobile template, cordova, phonegap, mobile, html" />
+    <title>Quang Group</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="" />
     <link rel="icon" type="image/png" href="{{ asset('mobile/assets/img/favicon.png')}}" sizes="32x32">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('mobile/assets/img/icon/192x192.png')}}">
     <link rel="stylesheet" href="{{ asset('mobile/assets/css/style.css')}}">
