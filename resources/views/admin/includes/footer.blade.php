50db53bc2fff71b5822fcb999cad2cd7253db7e9
vovantuan2002 <votuant2@gmail.com>
2022-05-23 13:33:17 +0700

    magnific-popup-image

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -14,13 +14,5 @@
     </li>
   </ul>
   <div class="copyright"> Copyright © 2022. All right reserved. </div>
-  <!-- Optional JavaScript -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js">
-  </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js">
-  </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js" integrity="sha512-C1zvdb9R55RAkl6xCLTPt+Wmcz6s+ccOvcr6G57lbm8M2fbgn2SUjUJbQ13fEyjuLViwe97uJvwa1EUf4F1Akw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
+ 
 </footer>
