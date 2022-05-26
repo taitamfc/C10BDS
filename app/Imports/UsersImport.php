881fd0d63279e2e8ae47188ffa23535f8b4696f3
taitamfc <taitam8590@gmail.com>
2022-05-26 20:07:53 +0700

    merge import user

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -18,29 +18,10 @@ class UsersImport implements ToCollection
     {
         foreach ($rows as $key => $row)
         {
-<<<<<<< HEAD
             if($key == 0 ){
                 continue ;
             }
             dispatch( new ProcessImportUser($row,$this->request->user_group_id,$this->request->branch_id) );
-=======
-            if($key == 0) continue;
-
-            $user = new User();
-            $user->name             = $row[1];
-            $user->gender           = 'male';
-            $user->address          = 'QUảng trị';
-            $user->email            = $row[2].'@gmail.com';
-            $user->phone            = '0'.$row[2];
-            $user->password         = Hash::make(123456);
-            $user->user_group_id    = $this->request->user_group_id;
-            $user->branch_id        = $this->request->branch_id;
-            $user->province_id      = 1;
-            $user->district_id      = 1;
-            $user->ward_id          = 1;
-            $user->note             = $row[3];
-            $user->save();
->>>>>>> ebc3360a1843971ce19dde2f5348a0a8f3852298
         }
     }
 }
